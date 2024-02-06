a89459851d5d1843a7b1f6fb82a2f60e82c62d1e
roxgiba <rox.giba@gmail.com>
2024-02-06 12:43:44 +0100

    Api connected, results display correctly

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
-<script setup></script>
+<script setup>
+import AutoComplete from './components/AutoComplete.vue'
+</script>
 
 <template>
   <header class="text-center bg-[#6bbbae]">
     <h1 class="text-white italic text-3xl font-bold md:text-5xl">vanWave</h1>
     <p class="text-sm md:text-xl">*study case for RoadSurfer</p>
   </header>
-  <main></main>
+  <main>
+    <AutoComplete />
+  </main>
 </template>
 
 <style scoped>
---
+++ src/components/AutoComplete.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {
+      inputValue: '',
+      results: []
+    }
+  },
+  methods: {
+    search() {
+      const api = `https://605c94c36d85de00170da8b4.mockapi.io/stations?search=${this.inputValue}`
+      fetch(api)
+        .then((response) => response.json())
+        .then((data) => {
+          this.results = data
+        })
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="text-center m-5">
+    <label>Search for station: </label>
+    <input v-model="inputValue" @input="search" class="border-2 rounded border-blue-700" />
+  </div>
+  <div>
+    <ul>
+      <li></li>
+    </ul>
+  </div>
+</template>
