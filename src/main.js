a5eb339fedabb4078d759aab1a89da3dd7660eaf
roxgiba <rox.giba@gmail.com>
2024-02-07 14:11:26 +0100

    improving performance

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
 import './assets/main.css'
-import { Calendar, DatePicker } from 'v-calendar'
+// import { Calendar, DatePicker } from 'v-calendar'
 import 'v-calendar/style.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 
 const app = createApp(App)
 
-app.use(createPinia())
-
-app.component('VCalendar', Calendar)
-app.component('VDatePicker', DatePicker)
+// app.component('VCalendar', Calendar)
+// app.component('VDatePicker', DatePicker)
 
 app.mount('#app')
