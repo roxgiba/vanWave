ad1999c5b1dbee7eb0e78c22ec6670b1aa808921
roxgiba <rox.giba@gmail.com>
2024-02-07 15:20:43 +0100

    fixing font size

--- src/components/AutoComplete.vue
+++ src/components/AutoComplete.vue
@@ -106,9 +106,7 @@ export default {
     </div>
     <!-- WEEK VIEW -->
     <div v-if="selectedStation" class="mt-10 bg-stone-300/80 p-3 rounded-lg">
-      <h2 class="text-base md:text-2xl font-bold pb-2">
-        Week View for: {{ selectedStation.name }}
-      </h2>
+      <h2 class="text-xl md:text-2xl font-bold pb-2">Week View for: {{ selectedStation.name }}</h2>
       <div id="calendar-like">
         <div>
           <button class="text-xs pr-5">&lt; previous</button>
@@ -117,7 +115,7 @@ export default {
         </div>
         <div id="week-view" class="m-8 grid grid-cols-1 md:grid-cols-7 gap-4">
           <div v-for="day in weekDays" :key="day" class="day-tile rounded-md">
-            <h3 className="font-bold text-lg">{{ day }}</h3>
+            <h3 className="font-bold text-lg bg-white/50 ">{{ day }}</h3>
             <ul>
               <li
                 v-for="booking in getBookingsForDay(day)"
